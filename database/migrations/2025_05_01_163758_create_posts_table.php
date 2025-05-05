1529158a04ce9b26124412df38f2a4a5a0ae2260
Azizbek <azizbekbazarbaev0606@gmail.com>
2025-05-05 16:19:08 +0500

    table is connect database

--- database/migrations/2025_05_01_163758_create_posts_table.php
+++ database/migrations/2025_05_01_163758_create_posts_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
-            
+
             $table->text('body');
             $table->timestamps();
+            
         });
     }
     
